8060ec97d85c2a60b61a4488e6f4a78a174dc534
JD Russo <jdr033@bucknell.edu>
2016-05-31 16:53:38 -0400

    Added theoretical model plot

--- dev/gillespie.py
+++ dev/gillespie.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # Python implementation of Gillespie algorithm
-#   for population dynamics simulation.
+#   for results dynamics simulation.
 # - https://people.maths.ox.ac.uk/erban/Education/StochReacDiff.pdf
 # - Heiko Reiger "Kinetic Monte Carlo" slides
 
@@ -9,7 +9,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Time to run
-T_MAX = 10
+T_MAX = 2
 
 # Current time
 t = 0
@@ -19,16 +19,17 @@ t = 0
 # TODO: Currently must be set from smallest to largest
 
 # Reaction 1: X -> 0
-mu1 = .08
+mu1 = .1
 # Reaction 2: X -> X + X
-mu2 = .1
+mu2 = .9
 
 mu = np.array([mu1, mu2])
 
 # Initialize populations
-N_x = 10
+N_0 = 10
+N_x = N_0
 
-population = []
+results = []
 while t < T_MAX:
 
     # Execute Gillespie algorithm
@@ -60,11 +61,23 @@ while t < T_MAX:
     tau = - np.log(r1)/sum(a)
 
 
+    results.append((t, N_x))
     # Increment timestep
     t += tau
 
     print(N_x)
-    population.append(N_x)
 
-plt.plot(population)
+print("Finished in %d steps." % len(results))
+
+# Plot data
+t = [x[0] for x in results]
+pop = np.array([x[1] for x in results])
+plt.plot(t, pop)
+
+# Generate theoretical model
+theoretical = N_0 * np.exp(t)
+plt.plot(t, theoretical)
+
+x = np.arange(0,10)
+
 plt.show()
