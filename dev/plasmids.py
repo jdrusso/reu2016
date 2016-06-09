7b7b4edfa54fd5605b1a7a02028f9ebd74d36d3c
JD Russo <jdr033@bucknell.edu>
2016-06-09 11:29:36 -0400

    Added functions to draw line plots of X vs mu1/mu2 and X vs alpha

--- dev/plasmids.py
+++ dev/plasmids.py
@@ -64,7 +64,7 @@ def gillespie(mu1=.7, mu2=.4, alpha=.2, t_max=T_MAX):
         # Reaction 1
         if r1*a0 < a[0]:
 
-            N_x -= 1.
+            N_x += 1.
 
         # Reaction 2
         elif r1 * a0 < a[0] + a[1]:
@@ -94,8 +94,10 @@ def gillespie(mu1=.7, mu2=.4, alpha=.2, t_max=T_MAX):
         results.append([t, N_x, N_y])
 
     # print("Finished in %d steps." % len(results))
-
+    # Store end conditions
     final = [alpha, mu1/mu2, N_x, N_y]
+
+    # Returns [t, N_x, N_y], [alpha, mu1/mu2, N_x, N_y]
     return results, final
 
 
@@ -119,6 +121,9 @@ def mu_vs_alpha(RESOLUTION=64, m2=[.3, .7], a=[.2, .7], mu1=.8, t_max=T_MAX):
             results.append(temp[1])
 
             c += 1
+
+            # If you remove this print statement, the code will throw errors :(
+            # TODO: Why does the code throw errors if you remove this print?
             print("Finished %d of %d" % (c, RESOLUTION**2))
 
     temp = [list(t) for t in zip(*results)]
@@ -135,6 +140,116 @@ def mu_vs_alpha(RESOLUTION=64, m2=[.3, .7], a=[.2, .7], mu1=.8, t_max=T_MAX):
         z += [float(temp[2][v]) / float(temp[2][v]+temp[3][v])]
 
     return x, y, z
+
+def x_vs_alpha(mu2s, a, mu1, RESOLUTION=64, t_max=T_MAX):
+
+    # results = []
+
+    X = []
+    Y = []
+    Z = []
+
+    for mu2 in mu2s:
+        c = 0
+        results = []
+
+        for alpha in a:
+            print(alpha)
+            temp = gillespie(mu1, mu2, alpha, t_max=t_max)
+
+            # Store [alpha, mu1/mu2, N_x, N_y] in results
+            results.append(temp[1])
+
+            c += 1
+
+            # print("Finished %d of %d" % (c, RESOLUTION**2))
+
+        # Unzip results into temp[0] = list of alpha values, temp[1] = list of
+        #   mu1/mu2, etc
+        temp = [list(t) for t in zip(*results)]
+
+        # X population
+        x = temp[0]
+        # alpha
+        y = [0]*len(x)
+        # mu1/mu2
+        z = temp[1]
+
+        for v in range(len(x)):
+            if temp[3][v] == 0:
+                y[v] = 1.0
+                continue
+
+            #Set Z to be the percentage of non-carrier bacteria
+            y[v] = float(temp[2][v]) / float(temp[2][v]+temp[3][v])
+
+        X += [x]
+        Y += [y]
+        Z += [z]
+
+
+    plt.xlabel(r"$\alpha$", fontsize=24)
+    plt.title(r"X population vs. $\alpha$ at $t=%.2f$" % T_MAX)
+    plt.ylim([0.7,1.])
+
+    linePlotData(X, Y, Z, mu1=.8, t_max=T_MAX, title=r"$\mu_1 / \mu_2$")
+
+    return X, Y, Z
+
+def x_vs_mu(mu2s, a, mu1, RESOLUTION=64, t_max=T_MAX):
+
+    # results = []
+
+    X = []
+    Y = []
+    Z = []
+
+    for alpha in a:
+        c = 0
+        results = []
+
+        for mu2 in mu2s:
+            temp = gillespie(mu1, mu2, alpha, t_max=t_max)
+
+            # Store [alpha, mu1/mu2, N_x, N_y] in results
+            results.append(temp[1])
+
+            c += 1
+
+            # print("Finished %d of %d" % (c, RESOLUTION**2))
+
+        # Unzip results into temp[0] = list of alpha values, temp[1] = list of
+        #   mu1/mu2, etc
+        temp = [list(t) for t in zip(*results)]
+
+        # mu1/mu2
+        x = temp[1]
+        # X population
+        y = [0]*len(x)
+        # alpha
+        z = temp[0]
+
+        for v in range(len(x)):
+            if temp[3][v] == 0:
+                y[v] = 1.0
+                continue
+
+            #Set Z to be the percentage of non-carrier bacteria
+            y[v] = float(temp[2][v]) / float(temp[2][v]+temp[3][v])
+
+        X += [x]
+        Y += [y]
+        Z += [z]
+
+
+    plt.legend(loc='lower left', title=r"$\alpha$")
+    plt.xlabel(r"$mu_1 / mu_2$", fontsize=24)
+    plt.title(r"X population vs. $mu_1 / mu_2$ at $t=%.2f$" % T_MAX)
+    # plt.ylim([0.7,1.])
+
+    linePlotData(X, Y, Z, mu1=.8, t_max=T_MAX, title = r"$\alpha$")
+
+    return X, Y, Z
 
 def plotData(x, y, z, RESOLUTION=64):
     # ###############
@@ -186,7 +301,8 @@ def plotData(x, y, z, RESOLUTION=64):
 
     plt.show()
 
-def animate(i, X, Y, Z, ax, z, t_max):
+# Contour plot
+def animate1(i, X, Y, Z, ax, z, t_max):
     # Clean up axes - NEED THIS!
     ax.cla()
 
@@ -213,16 +329,88 @@ def animate(i, X, Y, Z, ax, z, t_max):
 
     return cont,
 
+# Interpolated contour plot
+def animate2(i, X, Y, Z, ax, z, t_max):
+    res = 128
+    ax.cla()
+
+    # Pick out the X, Y, Z data corresponding to the ith time
+    x = X[i]
+    y = Y[i]
+    z = Z[i]
+
+    #########
+    # code for contour plot
+    print("[%d/%d] Beginning contour plotting..." % (i, t_max[1]))
+    xi, yi = np.linspace(min(x), max(x), res), np.linspace(min(y), max(y), res)
+    xi, yi = np.meshgrid(xi, yi)
+
+    print("[%d/%d] Interpolating..." % (i, t_max[1]))
+    rbf = scipy.interpolate.Rbf(x, y, z, function='linear')
+    zi = rbf(xi, yi)
+
+    print("[%d/%d] Plotting..." % (i, t_max[1]))
+    im = plt.imshow(zi, vmin=min(z), vmax=max(z), origin='lower',
+               extent=[min(x), max(x), min(y), max(y)], aspect="auto")
+    plt.scatter(x, y, c=z)
+
+
+    # Hack to print the color bar. If it's done every loop, it duplicates.
+    if i == 1:
+        plt.colorbar(im, label="X percentage of population")
+    plt.title("T = %.2f" % ((float(i) / t_max[1]) * t_max[0]))
+    plt.xlabel("alpha")
+    plt.ylabel("mu1 / mu2")
+    #########
+
+    return im,
+
+
+# 3D Surface plot
+def animate3(i, X, Y, Z, ax, z, t_max):
+    print("[%d/%d] Beginning contour plotting..." % (i, t_max[1]))
+    res = 128
+
+    # Pick out the X, Y, Z data corresponding to the ith time
+    x = X[i]
+    y = Y[i]
+    z = Z[i]
+
+    ###############
+    # Code for surface plot
+    #Reshape data for plotting
+    cols = np.unique(x).shape[0]
+    X = np.array(x).reshape(-1, cols)
+    Y = np.array(y).reshape(-1, cols)
+    Z = np.array(z).reshape(-1, cols)
+
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
+    # ax = fig.gca(projection='3d')
+    ax.clear()
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, vmin=min(z),vmax=max(z), cmap=cm.coolwarm)
+
+    plt.title("T = %.2f" % ((float(i) / t_max[1]) * t_max[0]))
+    ax.set_xlabel("alpha")
+    ax.set_ylabel("mu1 / mu2")
+    ax.set_zlabel("X Population")
+    # plt.show()
+    ###############
+
+
+    return surf,
+
 # Function to draw and animate a contour plot of population vs
 def animateContour(m2=[.1, .7], a=[.1, .9], mu1=.8, RESOLUTION=128):
 
     # Set up plot
     fig = plt.figure()
+    # ax = plt.axes(xlim=tuple(a), ylim=tuple(m2), projection='3d')
     ax = plt.axes(xlim=tuple(a), ylim=tuple(m2))
 
     # Generate a range of times
     # Set max time, and number of steps
-    t_max = (3., 100.)
+    t_max = (5., 20.)
     times = np.linspace(0.1, t_max[0], t_max[1])
 
 
@@ -235,7 +423,8 @@ def animateContour(m2=[.1, .7], a=[.1, .9], mu1=.8, RESOLUTION=128):
         Z += [z]
 
     # Generate animation
-    anim = animation.FuncAnimation(fig, animate, frames=len(times), interval=150,
+
+    anim = animation.FuncAnimation(fig, animate1, frames=len(times), interval=150,
                 fargs=(X, Y, Z, ax, True, t_max), blit=False)
 
     # Write animation to MP4 using ffmpeg
@@ -244,14 +433,53 @@ def animateContour(m2=[.1, .7], a=[.1, .9], mu1=.8, RESOLUTION=128):
 
     return anim
 
+def linePlotData(xs, ys, zs, mu1, t_max, title):
+
+    colors = list('rgbymc')
+    c = 0
+
+    for mu2 in zs:
+
+        color = colors[c%len(colors)]
+
+        # Plot data
+        plt.plot(xs[c], ys[c], color+'o')
+
+        # Plot best fit line
+        plt.plot(xs[c], np.poly1d(np.polyfit(xs[c], ys[c], 1))(xs[c]),
+            color+'-',
+            label="%.3f" % zs[c][-1], linewidth=3)
+
+        c += 1
+
+    plt.legend(loc='lower left', title=title)
+
+    plt.xlim([min(xs[0]), max(xs[0])])
+    plt.ylabel("% X population", fontsize=16)
+
 def main():
     res = 64
 
+    ### Generate contour plot of mu vs alpha
     # x, y, z = mu_vs_alpha(m2=[.3, .7], a=[.2, .7], mu1=.8, RESOLUTION=res)
     # plotData(x, y, z, RESOLUTION=res)
 
-    a = animateContour(m2=[.3, .7], a=[.2, .7], mu1=.8, RESOLUTION=res)
-    return a
+    ### Generate animated contour plot of mu vs alpha
+    # a = animateContour(m2=[.6, .7], a=[.05, .1], mu1=.8, RESOLUTION=res)
+    # return a
+
+
+    # Generate line plot of x vs alpha. ys is a list of alpha values,
+    #    zs is a list of mu1/mu2 values
+    x_vs_alpha(mu2s=np.linspace(.7,.78,5), a=np.linspace(.01, .1, res), mu1=.8, RESOLUTION=res)
+
+
+
+    # Generate line plot of x vs alpha. ys is a list of alpha values,
+    #    zs is a list of mu1/mu2 values
+    x_vs_mu(mu2s=np.linspace(.7,.78,res), a=np.linspace(.01, .1, 5), mu1=.8, RESOLUTION=res)
+
+    plt.show()
 
 if __name__ == "__main__":
     a = main()
