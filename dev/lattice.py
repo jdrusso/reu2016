0d838a363d3a85bd50fc3e7620683f00733a9550
JD Russo <jdr033@bucknell.edu>
2016-07-26 16:43:43 -0400

    Optimizations, added daughter cell spreading to a nearby site, etc

--- dev/lattice.py
+++ dev/lattice.py
@@ -20,13 +20,15 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-S_0 = 100
-R_0 = 100
-K = 400
+S_0 = 500
+R_0 = 500
+K_SITE = 10 #Per site carrying capacity
 PLASMIDS = 1e4
-SPACING = .01
+SPACING = .1
 LATTICE_X = 100
 LATTICE_Y = 100
+NUM_SITES = LATTICE_X*LATTICE_Y
+K = K_SITE * NUM_SITES
 SYMMETRIC = True
 RECYCLING = False
 
@@ -48,10 +50,25 @@ def add_occupied(pos, occupied):
 
 def get_occupied(lattice, occupied, TYPE):
 
+    # Make list of valid occupied sites
+    # occu = [pos for pos in occupied[TYPE] if sum(lattice[pos[0],pos[1]]) < K_SITE and lattice[]]
+    # occu = list(filter(
+        # lambda x: True if sum(lattice[x[0],x[1]]) < K_SITE else False,
+        # lambda x: True if lattice[x[0],x[1]]) < K_SITE else True,
+        # occupied[TYPE]))
+
     # Generate a probability distribution for picking a site using number of particles at site
-    distribution = list([lattice[pos[0],pos[1],TYPE] for pos in occupied[TYPE]])
+    distribution = [lattice[pos[0],pos[1],TYPE] for pos in occupied[TYPE]]
+
+    # This ensures the probability distribution is seen by numpy as nonnegative.
+    #   Without this, differences in precision can make numpy think a probability
+    #   is negative.
+    # print(len(distribution))
+
     # Normalize distribution
-    distribution /= sum(distribution)
+    distribution = [x + 1e-5 for x in distribution]
+    num = sum(distribution)
+    distribution /= num
 
     # Randomly select an occupied lattice site using the distribution
     choice = np.random.choice(len(occupied[TYPE]), p=distribution)
@@ -61,18 +78,36 @@ def get_occupied(lattice, occupied, TYPE):
 
     return choice
 
+def placeChild(pos, lattice, TYPE):
+    x, y = pos[0], pos[1]
+
+    direction = np.random.choice(range(5))
+
+    if direction == 0:
+        y += 1
+    elif direction == 1:
+        y -= 1
+    elif direction == 2:
+        x += 1
+    elif direction == 3:
+        x -= 1
+    elif direction == 4:
+        pass
+
+    # Check if selected location is at carrying capacity yet.
+
+    lattice[x%LATTICE_X,y%LATTICE_Y,TYPE] += 1
+
+    return
+
 
 def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=PLASMIDS, num=0):
 
+    print("r0 is %d" % r0)
+
     plt.ion()
 
-    # If this was any more of a ballpark, I'd need some crackerjacks
-    time = 12.4748 + .00824437 * (LATTICE_X * LATTICE_Y)
-    t_sec = time % 60
-    t_min = time // 60
-    print('Estimated time is %d min %d sec ' % (t_min, t_sec))
-
-    print("Beginning")
+    print("Beginning with K_SITE = %d" % K_SITE)
 
     # Set up progress bar, if applicable
     if PBAR:
@@ -108,31 +143,6 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
         lattice[x,y,R] += 1
         add_occupied([x,y], occupied[R])
 
-    for i in range(int(r0)):
-        x = np.random.randint(0,len(lattice))
-        y = np.random.randint(0,len(lattice[0]))
-        lattice[x,y,R] += 1
-        add_occupied([x,y], occupied[R])
-
-    xdata, ydata, zdata = [], [], []
-    for x in range(len(lattice)):
-        for y in range(len(lattice[x])):
-            xdata += [x]
-            ydata += [y]
-            zdata += [lattice[x,y,R]/lattice[x,y,S]]
-
-
-    # Generate list of valid S and R positions
-    valid_S, valid_R = [], []
-    probability = []
-    for x in range(len(lattice)):
-        for y in range(len(lattice[x])):
-            if lattice[x,y,S] > 0:
-                valid_S += [(x,y)]
-                # probability += [lattice[x,y,TYPE]/num]
-            if lattice[x,y,R] > 0:
-                valid_R += [(x,y)]
-
     # Initialize some relevant parameters
     t = 0.
     cur_t = 1e10
@@ -145,14 +155,15 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
     Writer = animation.writers['ffmpeg']
     writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
 
-
+    N_s = s0
+    N_r = r0
 
 
 
     while t < t_max:
 
-        N_s = sum(sum(lattice[:,:,S]))
-        N_r = sum(sum(lattice[:,:,R]))
+        # N_s = sum(sum(lattice[:,:,S]))
+        # N_r = sum(sum(lattice[:,:,R]))
 
 
         #########################
@@ -176,7 +187,8 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
             #Pick a lattice position to update
             x, y = choosePoint(lattice, S, occupied)
 
-            lattice[x,y,S] += 1.
+            placeChild((x, y), lattice, S)
+            N_s += 1
 
         # Reaction 2: S -> R
         elif r1 * a0 < sum(a[:2]) and N_s > 0:
@@ -184,20 +196,29 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
             x, y = choosePoint(lattice, S, occupied)
 
             lattice[x,y,S] -= 1.
-            lattice[x,y,R] += 1.
+            # lattice[x,y,R] += 1.
+            placeChild((x, y), lattice, R)
+
             plasmids -= 1.
+            N_s -= 1
+            N_r += 1
 
         # Reaction 3: R -> R + S
         elif r1 * a0 < sum(a[:3]):
 
             x, y = choosePoint(lattice, R, occupied)
 
+            # R -> R+R       Symmetric
             if SYMMETRIC:
-                # Symmetric division - conserves plasmid number
-                lattice[x,y,S] += 1.
+                # lattice[x,y,R] += 1.
+                placeChild((x, y), lattice, R)
+                N_r += 1
+            # R -> R+S       Asymmetric division
             else:
-                # Asymmetric division
-                lattice[x,y,R] += 1.
+                # lattice[x,y,R] += 1.
+                placeChild((x, y), lattice, S)
+                N_s += 1
+
 
         # Reaction 4: R -> 0
         elif r1 * a0 < sum(a[:4]):
@@ -206,6 +227,7 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
 
             if lattice[x,y,R] >= 1:
                 lattice[x,y,R] -= 1.
+                N_r -= 1
                 if RECYCLING:
                     plasmids += 1.
 
@@ -228,7 +250,8 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
         # Only save data if a certain interval between datapoints is met
         if cur_t > SPACING:
             print("[ %.2f%% ]" % (100*t/t_max), end='  ')
-            print(t)
+            print("%.4f" % t, end='\t')
+            print("Ns = %d, Nr = %d" % (N_s, N_r))
             ns = N_s
             nr = N_r
             if ns == 1:
@@ -240,8 +263,10 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
 
             # Plot results
             plt.gcf().suptitle("T = %.4f" % t)
-            heatmap_S = axarr[S].imshow(lattice[:,:,S], cmap='Blues', interpolation='none')
-            heatmap_R = axarr[R].imshow(lattice[:,:,R], cmap='Reds', interpolation='none')
+            heatmap_S = axarr[S].imshow(lattice[:,:,S], cmap='Reds', interpolation='nearest', vmin=0, vmax=K_SITE)
+            # print(lattice[1:20,1:20,S])
+            # print(lattice[:,:,R])
+            heatmap_R = axarr[R].imshow(lattice[:,:,R], cmap='Blues', interpolation='none', vmin=0, vmax=K_SITE)
             axarr[S].set_title("S Population")
             axarr[R].set_title("R Population")
             # ims_S.append([heatmap_S])
@@ -259,9 +284,9 @@ def gillespie(mu1, mu2, alpha, d, t_max, q=False, s0 = S_0, r0 = R_0, _PLASMIDS=
             # plt.pause(.01)
 
     print("Generating animation .")
-    # s_im_ani = animation.ArtistAnimation(plt.figure(1), ims_S, interval=50, repeat_delay=1000, blit=False)
-    # r_im_ani = animation.ArtistAnimation(plt.figure(1), ims_R, interval=50, repeat_delay=1000, blit=False)
-    a_im_ani = animation.ArtistAnimation(plt.figure(1), ims_a, interval=50, repeat_delay=1000, blit=True)
+    # s_im_ani = animation.ArtistAnimation(fig, ims_S, interval=50, repeat_delay=1000, blit=False)
+    # r_im_ani = animation.ArtistAnimation(fig, ims_R, interval=50, repeat_delay=1000, blit=False)
+    a_im_ani = animation.ArtistAnimation(fig, ims_a, interval=100, repeat_delay=1000, blit=False)
     # s_im_ani.save("s_pop.mp4", writer=writer)
     # r_im_ani.save("r_pop.mp4", writer=writer)
     a_im_ani.save("a_pop.mp4", writer=writer)
@@ -343,7 +368,7 @@ def runsim(alphas, mu1, mu2s, d, t_max, filename):
 
 def main():
 
-    runsim(alphas=[.2], mu1=.8, mu2s=[.72], d=.3, t_max=3, filename='graphics/lattice.dat')
+    runsim(alphas=[.2], mu1=.8, mu2s=[.72], d=.3, t_max=5, filename='graphics/lattice.dat')
     return
 
 
